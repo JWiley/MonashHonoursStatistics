75a27c3a1159e638e99dd11af58909ecd4ad398e
Joshua F. Wiley <jwiley.psych@gmail.com>
2019-04-06 08:26:30 +1100

    added setup for model comparisons

---
+++ LMM_Comparison_worksheet.R
@@ -0,0 +1,28 @@
+#### 1. Setup (You Try It) ####
+
+## to start with, we will load a package for data management
+## and load a package for fitting Linear Mixed effects Models (LMMs)
+
+## recall that loading a package is like opening an app
+## and you need to repeat this process each time you start up R
+## if everything installed successfully already
+## this code should work and return no errors
+## if this does not work, try to install it first
+## by uncommenting the install packages code
+
+# install.packages("data.table", dependencies = TRUE)
+# install.packages("lme4", dependencies = TRUE)
+# install.packages("ggplot2", dependencies = TRUE)
+# install.packages("visreg", dependencies = TRUE)
+library(data.table)
+library(lme4)
+library(ggplot2)
+library(visreg)
+
+## load the data (note this is already an R dataset)
+## so we use a new function readRDS()
+## make sure that the data file is located in
+## your project directory
+d <- readRDS("aces_daily_sim_processed.RDS")
+
+
