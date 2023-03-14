1b721a9972011b6cc53fa42ea57aa4da9e0a78be
Michelle Byrne <michelle.byrne@gmail.com>
2023-03-14 13:29:16 +1100

    add zip files for HTML

---
+++ Week1_worksheet.R
@@ -0,0 +1,189 @@
+#### 1. Basic R Use ####
+
+## use R as a calculator
+3 + 4 # addition
+
+2 * 6 # multiplication
+
+4 / 2 # division
+
+2^3 # powers
+
+## create a vector
+c(1, 5, 4)
+
+## assign results to an object
+## note that after it is assigned, it shows up in RStudio in the "Environment"
+x <- c(1, 3, 5)
+
+## view an existing object
+x
+
+## round data (helpful for reporting)
+## the first argument is the number to round
+## the second argument is how many digits to use for rounding
+round(1.214294254, digits = 2)
+
+#### 2. Descriptive Statistics ####
+
+## calculate the mean
+## note that we reuse the previously assigned variable, x
+mean(x)
+
+## calculate the median
+median(x)
+
+## calculate the standard deviation
+sd(x)
+
+## minimum
+min(x)
+
+## maximum
+max(x)
+
+## if there are missing values, R will not calculate
+## to see this, lets first create an object with some missing data
+## we will call it "y"
+## NA stands for Not Available, i.e., missing
+## If we want the vector to have the numbers 1, 3, missing, then 7, 
+## what would we fill in the ??? with here?
+y <- ???
+
+## calculate mean on y
+mean(y)
+
+## Why didn't that work? We need to tell R
+## to remove missing values first
+## (na for not available; rm for remove)
+## by adding an argument, na.rm = TRUE
+mean(y, na.rm = TRUE)
+
+#### 2b. You Try It ####
+
+## find the mean of these numbers: 5, 3, 2, 9, 1
+???
+  
+## find the standard deviation of the variable "y"
+???
+
+#### 3. Using a Dataset ####
+
+## R has a built in dataset called "mtcars"
+## this dataset has variables on about 32 different cars
+
+## view the dataset
+View(mtcars)
+
+## one the variables in the dataset is how many
+## miles per gallon of petrol each car gets
+## this variable is called "mpg"
+## to access the variable from within the mtcars dataset
+## we use the "$" operator
+## the code below accesses and prints all the observations
+## from the mpg variable
+mtcars$mpg
+
+## note what happens if the case is wrong
+## because a variable called "Mpg" does not exist
+## R return NULL indicating no data
+mtcars$Mpg
+
+## it is also possible to round an entire set of numbers
+round(mtcars$mpg, digits = 0)
+
+
+#### 3b. You Try It ####
+## calculate descriptive statistics (mean, standard deviation) of the 
+## variable mpg
+???
+
+## How do you get a summary of a whole dataset? Use google or the ? function
+???
+
+#### 4. Loading Data ####
+
+## to start with, we will load a package for data management.
+## Loading a package/library is like opening an app
+## and you need to repeat this process each time you start up R
+## note that if this does not work, try to install it first
+## by uncommenting the install packages code
+
+# install.packages("data.table", dependencies = TRUE)
+library(data.table)
+
+## import CSV - first insert your working directory instead of mine
+setwd("C:/Users/michelle/Documents/git_repos/MonashHonoursStatistics")
+d <- read.csv("IntroR_sample.csv")
+
+# (When you have more time, also install package "tidyverse"
+# - it takes a while)
+
+## YOU TRY IT: get a summary of the data
+???
+
+## load package to read Excel files (.xls or .xlsx)
+## if not installed please uncomment and run code below
+ install.packages("xlsx", dependencies = TRUE)
+library("xlsx")
+
+## read in the "Sleep" sheet
+d.acti <- read.xlsx2("actigraph_scored_31.xlsx", sheetName = "Sleep")
+
+## view the variable names in the dataset
+names(d.acti)
+
+#### 4b. You Try It ####
+## calculate the mean sleep duration (total sleep time; TST)
+???
+# what error did you get?
+
+# Search the following functions
+?str
+?as.numeric
+#and see if you can figure out how to fix it
+???
+???
+???
+
+
+## view the Sleep Efficiency (se) variable in d.acti
+## what is the second value?
+???
+  
+## what are the variable names in the "Summary" sheet?
+???
+
+
+#### 5. Logical Operators ####
+
+## "==" : logical test if Depressed is equal to 1
+d$Depressed == 1
+
+## ">" : logical test whether zStress is greater than 0
+d$zStress > 0
+
+## "|" : logical "or"; test whether either condition is TRUE
+## depressed or high stress
+d$Depressed == 1 | d$zStress > 1
+
+## "&" : logical "and"; test whether both conditions are TRUE
+## depressed and low stress
+d$Depressed == 1 & d$zStress < 1
+
+## we can use square brackets, [], to subset a variable or dataset
+## we can subset by number or by logical value.
+## here are all the values for zStress
+d$zStress
+
+## here is just the first and third value
+d$zStress[c(1, 3)]
+
+## YOU TRY IT: What is the 10th value?
+???
+
+## here are just the values of zStress where Depressed == 1
+d$zStress[d$Depressed == 1]
+
+## here are just the values of zStress where zStress > 1
+d$zStress[d$zStress > 1]
